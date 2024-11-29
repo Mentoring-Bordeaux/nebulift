3d0a24f2356abc48e72b934a25e2d6519fe9f4dd
Arthur Le Floch <arthur.le_floch@bordeaux-inp.fr>
2024-11-29 22:20:30 +0100

    test: pass data using env vars

--- templates/poc-template/index.ts
+++ templates/poc-template/index.ts
@@ -1,9 +1,13 @@
-import * as pulumi from "@pulumi/pulumi";
 import * as github from "@pulumi/github";
-import * as fs from "fs";
 
-// Define the repository and branch where the file will be uploaded
-const repositoryName = "poc-template";
+// Get data from the environment variables
+const data = process.env.DATA;
+if (!data) {
+    throw new Error("DATA env var is required");
+}
+const parsedData = JSON.parse(data);
+
+const repositoryName = parsedData.repositoryName;
 
 // Create a new GitHub repository
 const repo = new github.Repository("repo", {
