33c53d205e8811e583b2eca556fc9e7f26b44872
TstXerox <jouahriadib+github@gmail.com>
2024-12-06 17:13:54 +0100

    Test: test template execution with multiple stacks

--- gen_csharp/Program.cs
+++ gen_csharp/Program.cs
@@ -1,15 +1,24 @@
 ﻿using Pulumi.Automation;
 using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 using System.Diagnostics;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 class Program
-{
+{  
     static async Task Main(string[] args)
     {
         try
         {   
+            var pulumiUser = Environment.GetEnvironmentVariable("PULUMI_USER");
+            if (string.IsNullOrEmpty(pulumiUser))
+            {
+                Console.WriteLine("Erreur : La variable d'environnement PULUMI_USER n'est pas définie.");
+                return;
+            }
+            Console.WriteLine("Pulumi user: " + pulumiUser);
+
             var githubToken = Environment.GetEnvironmentVariable("GITHUB_TOKEN");
             if (string.IsNullOrEmpty(githubToken))
             {
@@ -17,19 +26,27 @@ class Program
                 return;
             }
 
-            var stackName = "Adib0/poc-template/dev";
+            var inputs = new Inputs
+            {
+               macros = new Macros
+               {
+                   projectName = "Poc options C#",
+                   projectDescription = "A configurable template for Nebulift executed with C#"
+               },
+               constants = new Constants
+               {
+                   repositoryName = "Nebulift_Template",
+                   privateRepository = false
+               },
+               env = new Dictionary<string, object>()
+            };
+
+            var inputsJson = JsonSerializer.Serialize(inputs);
+            Console.WriteLine("Inputs: " + inputsJson);
+
+            var stackName = $"{pulumiUser}/poc-options/dev3";
             var url = "https://github.com/Mentoring-Bordeaux/nebulift.git";
-
-            // var startInfo = new ProcessStartInfo
-            // {
-            //     FileName = "dotnet",
-            //     Arguments = "run",
-            //     UseShellExecute = false,
-            //     RedirectStandardOutput = true,
-            //     RedirectStandardError = true
-            // };
-            // startInfo.EnvironmentVariables["GITHUB_TOKEN"] = githubToken;
-
+            
             var gitAuth = new RemoteGitAuthArgs
             {
                 PersonalAccessToken = githubToken
@@ -37,9 +54,15 @@ class Program
 
             var stackArgs = new RemoteGitProgramArgs(stackName, url) 
             {
-                ProjectPath = "templates/poc-template",
+                ProjectPath = "templates/poc-options",
                 Branch = "feature/template-execution",
                 Auth = gitAuth
+            };
+            
+            var envValue = new EnvironmentVariableValue(inputsJson);
+            stackArgs.EnvironmentVariables = new Dictionary<string, EnvironmentVariableValue>
+            {
+                { "NEBULIFT_INPUTS", envValue }
             };
             
             var stack = await Pulumi.Automation.RemoteWorkspace.CreateOrSelectStackAsync(stackArgs);
@@ -49,7 +72,7 @@ class Program
             var previewResult = await stack.PreviewAsync();
             Console.WriteLine("End of preview");
             Console.WriteLine("Preview result: " + Newtonsoft.Json.JsonConvert.SerializeObject(previewResult, Newtonsoft.Json.Formatting.Indented));
-
+            // Console.WriteLine("Preview result: " + previewResult);
 
             // Apply the changes
             Console.WriteLine("Running update ...");
@@ -66,4 +89,23 @@ class Program
             Console.WriteLine("Stack trace: " + ex.StackTrace);
         }
     }
+}
+
+class Inputs
+{
+   public required Macros macros { get; set; }
+   public required Constants constants { get; set; }
+   public required Dictionary<string, object> env { get; set; }
+}
+
+class Macros
+{
+   public string projectName { get; set; } = string.Empty;
+   public string projectDescription { get; set; } = string.Empty;
+}
+
+class Constants
+{
+   public string repositoryName { get; set; } = string.Empty;
+   public bool privateRepository { get; set; } = false;
 }
