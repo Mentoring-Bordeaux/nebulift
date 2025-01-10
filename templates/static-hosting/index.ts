f4062ceb0b7b6b56635b6a54ffe7b8df4fa42dec
Arthur Le Floch <arthur.le_floch@bordeaux-inp.fr>
2025-01-10 17:09:16 +0100

    test: only resource group

--- templates/static-hosting/index.ts
+++ templates/static-hosting/index.ts
@@ -11,15 +11,3 @@ const repositoryName = "static-hosting";
 const resourceGroup = new azure.resources.ResourceGroup("resourceGroup", {
     location: "WestEurope",
 });
-
-// Create a Storage Account to save an index.html file
-const storageAccount = new azure.storage.StorageAccount("nebuliftstorage", {
-    resourceGroupName: resourceGroup.name,
-    allowBlobPublicAccess: true,
-    sku: {
-        name: azure.storage.SkuName.Standard_LRS,
-    },
-    kind: azure.storage.Kind.StorageV2,
-});
-
-export const staticEndpoint = storageAccount.primaryEndpoints.web;
