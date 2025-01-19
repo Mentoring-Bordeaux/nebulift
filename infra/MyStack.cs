f6515bd27a0ca91c8c3785ff150219a5af1de165
Simon DUCAMP <ducampsimon@outlook.fr>
2025-01-19 15:02:29 +0100

    fix(infra): wapp now also depends on registry name

--- infra/MyStack.cs
+++ infra/MyStack.cs
@@ -49,7 +49,7 @@ internal class MyStack : Stack
             ServerFarmId = appServicePlan.Id,
             SiteConfig = new SiteConfigArgs
             {
-                LinuxFxVersion = "DOCKER|nebulift-backend:latest"
+                LinuxFxVersion = "DOCKER|nebulift.azurecr.io/nebulift-backend:latest"
             },
         });
 
